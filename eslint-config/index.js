8b7e3f029f9c765d25fa99bd67dae48aecb0e5b7
tihonove <tihonove@skbkontur.ru>
2016-05-02 19:49:39 +0500

    move eslint config to external package

--- eslint-config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "extends": [
-        "./base.js",
-        "./react.js",
-    ],
-    "rules": {},
-};
--- eslint-config/rules/strict.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "rules": {
-    // Disallow 'use strict' statement
-    // babel auto-inserts 'use strict';
-    // http://eslint.org/docs/rules/strict
-    "strict": [2, "never"],
-  },
-};
